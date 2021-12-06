7fafdda886e3766f1ed076261a216fbae2964428
vichuzhanin <93120035+vichuzhanin@users.noreply.github.com>
2021-12-06 20:49:56 +0300

    Update SQL_homework_02.sql

--- SQL/SQL_homework_02.sql
+++ SQL/SQL_homework_02.sql
@@ -1,8 +1,10 @@
+-- Создать таблицу employees
 create table employees(
 	employee_id serial primary key,
 	employee_name varchar (50) not null
 )
 
+-- Наполнить таблицу employee 70 строками
 insert into employees(employee_name)
 values ('Victoria Boyd'),
 ('Robert Gonzalez'),
@@ -75,11 +77,13 @@ values ('Victoria Boyd'),
 ('Ray Wright'),
 ('Kathryn Reid');
 
+-- Создать таблицу salary
 create table salary(
 	salary_id serial primary key,
 	monthly_salary int not null
 )
 
+-- Наполнить таблицу salary 15 строками
 insert into salary(monthly_salary)
 values (1000),
 (1100),
@@ -98,12 +102,14 @@ values (1000),
 (2400),
 (2500);
 
+-- Создать таблицу employee_salary
 create table employee_salary(
 	employee_salary_id serial primary key,
 	employee_id int unique not null,
 	salary_id int not null
 )
 
+-- Наполнить таблицу employee_salary 40 строками
 insert into employee_salary(employee_id, salary_id)
 values (63, 6),
 (62, 5),
@@ -146,14 +152,17 @@ values (63, 6),
 (79, 10),
 (80, 6);
 
+-- Создать таблицу roles
 create table roles(
 	roles_id serial primary key,
 	role_name int unique not null
 )
 
+-- Поменять тип столба role_name с int на varchar(30)
 alter table roles
 alter column role_name type varchar(30);
 
+-- Наполнить таблицу roles 20 строками
 insert into roles(role_name)
 values ('Junior Python developer'),
 ('Middle Python developer'),
@@ -176,6 +185,7 @@ values ('Junior Python developer'),
 ('Middle Automation QA engineer'),
 ('Senior Automation QA engineer');
 
+-- Создать таблицу roles_employee
 create table roles_employee(
 	roles_employee_id serial primary key,
 	employee_id int unique not null,
@@ -186,12 +196,60 @@ create table roles_employee(
 		references roles(role_id)
 )
 
+-- Наполнить таблицу roles_employee 40 строками
+insert into roles_employee(employee_id, role_id)
+values (7, 2),
+(20, 4),
+(3, 9),
+(5, 13),
+(23, 4),
+(11, 2),
+(10, 9),
+(22, 13),
+(21, 3),
+(34, 4),
+(6, 7),
+(66, 1),
+(42, 2),
+(17, 15),
+(14, 6),
+(13, 8),
+(68, 10),
+(2, 16),
+(9, 11),
+(70, 12),
+(16, 14),
+(47, 17),
+(64, 18),
+(58, 19),
+(40, 15),
+(59, 20),
+(15, 8),
+(50, 7),
+(12, 9),
+(27, 18),
+(51, 19),
+(61, 20),
+(43, 18),
+(30, 2),
+(41, 4),
+(38, 6),
+(39, 1),
+(67, 3),
+(8, 5),
+(28, 7);
+
+-- Проверка корректности создания таблицы employees
 select * from employees
 
+-- Проверка корректности создания таблицы salary
 select * from salary
 
+-- Проверка корректности создания таблицы employee_salary
 select * from employee_salary
 
+-- Проверка корректности создания таблицы roles
 select * from roles
 
+-- Проверка корректности создания таблицы roles_employee
 select * from roles_employee
